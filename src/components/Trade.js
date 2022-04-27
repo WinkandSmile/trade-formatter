380bb1820920c421c849d359f8c4f1c146b24299
Wink and Smile <102535468+WinkandSmile@users.noreply.github.com>
2022-04-27 13:31:10 +0200

    trade change comma

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -9,15 +9,15 @@ const Trade = ({ trade, onDelete }) => {
           />
           </h3>
         <p>@traderbeasts,</p>
-        <p>Unique trade ID: {trade.id},</p>
-        <p>Instrument: {trade.instrument},</p>             
-        <p>Entry Date/Time: {trade.entrydt},</p>
-        <p>Direction: {trade.direction},</p>    
-        <p>Entry Price: {trade.entryprice},</p>
-        <p>Price Target: {trade.takeprofit},</p>
-        <p>Stop Loss: {trade.stoploss},</p>
-        <p>Trade Account Risk (%): {trade.tarpercent},</p>   
-        <p>Good til Close Date/Time: {trade.gtcdt},</p>  
+        <p>Unique trade ID, {trade.id},</p>
+        <p>Instrument, {trade.instrument},</p>             
+        <p>Entry Date/Time, {trade.entrydt},</p>
+        <p>Direction, {trade.direction},</p>    
+        <p>Entry Price, {trade.entryprice},</p>
+        <p>Price Target, {trade.takeprofit},</p>
+        <p>Stop Loss, {trade.stoploss},</p>
+        <p>Trade Account Risk (%), {trade.tarpercent},</p>   
+        <p>Good til Close Date/Time, {trade.gtcdt},</p>  
     
         </div>
    
